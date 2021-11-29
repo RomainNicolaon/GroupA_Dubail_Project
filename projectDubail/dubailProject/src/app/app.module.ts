49c484c1d383e430c45d1777e4f970778214dfd8
Rasputin-6 <78964703+BrendonDesvaux@users.noreply.github.com>
2021-11-29 13:59:33 +0100

    update angular project
    added few components:
    - custom
    -custom-metal
    -custom-pierres

--- projectDubail/dubailProject/src/app/app.module.ts
+++ projectDubail/dubailProject/src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './home-component/footer/footer.component';
 import { HeaderComponent } from './home-component/header/header.component';
 import { BijouxComponent } from './bijoux/bijoux.component';
+import { OuvertureComponent } from './ouverture/ouverture.component';
+import { ThanksComponent } from './thanks/thanks.component';
+import { CustomEngraveComponent } from './custom-engrave/custom-engrave.component';
+import { CustomMetalComponent } from './custom-metal/custom-metal.component';
+import { CustomStoneComponent } from './custom-stone/custom-stone.component';
 
 const routes: Routes = [
-  { path: 'bijoux', component: BijouxComponent }
-  // {path:'home', redirectTo: ''},
- 
+  { path: 'bijoux', component: BijouxComponent },
+  { path: '', component:OuvertureComponent},
+  {path:'welcome', redirectTo: ''}
 ];
 
 @NgModule({
@@ -18,7 +23,12 @@ const routes: Routes = [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    BijouxComponent
+    BijouxComponent,
+    OuvertureComponent,
+    ThanksComponent,
+    CustomEngraveComponent,
+    CustomMetalComponent,
+    CustomStoneComponent
   ],
   imports: [
     BrowserModule,
---
+++ projectDubail/dubailProject/src/app/custom-engrave/custom-engrave.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-engrave',
+  templateUrl: './custom-engrave.component.html',
+  styleUrls: ['./custom-engrave.component.css']
+})
+export class CustomEngraveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
