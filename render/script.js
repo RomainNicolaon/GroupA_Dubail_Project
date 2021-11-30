2f5e4c30664e12c3ab845640080d3c5ea4ac9f4e
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2021-11-30 10:47:24 +0100

    I did an oopsi

---
+++ render/script.js
@@ -0,0 +1,15 @@
+let input1 = document.getElementById('send');
+let div1 = document.getElementById('final');
+
+function changeTexture() {
+    
+}
+
+function changeModel() {
+    
+}
+
+function changeText() {
+    div1.innerHTML = input1.value;
+    
+}
